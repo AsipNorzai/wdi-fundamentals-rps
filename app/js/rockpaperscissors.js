8902b152ffd11dcbc05ab6c39c3a4084d3c86482
Asip Norzai <asifkhannoorzai@gmail.com>
2015-09-27 18:31:57 +1000

    Working game after some hard thinking. Doesn't work on repl.it but works on lab on codecamdemy.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -7,6 +7,7 @@ function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
     return prompt();
 }
+
 function randomPlay() {
     var randomNumber = Math.random();
     if (randomNumber < 0.33) {
@@ -22,23 +23,17 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    if(move === null) {
-        move =  getInput;
-    }
-    return move;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
-  if(move === null) {
-      move = randomPlay;
-  }
-  return move;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
     var winner;
     if (playerMove === computerMove ) {
-      winner = 'tie'
+      winner = 'tie';
     } else if (playerMove === 'rock' && computerMove === 'scissors') {
       winner = "Player";
     } else if (playerMove === 'rock' && computerMove === 'paper') {
@@ -57,23 +52,34 @@ function getWinner(playerMove,computerMove) {
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    var playerTurn = getPlayerMove;
-    var computerTurn = getComputerMove;
-    var winner = getWinner(playerTurn, computerTurn);
-    while (playerWins < 5 || computerWins < 5) {
+    var playerWins = 0,
+    computerWins = 0;
+    while (playerWins < 5 && computerWins < 5) {
+        var  playerMove = getPlayerMove(),
+            computerMove = getComputerMove(),
+            winner = getWinner(playerMove, computerMove);
       if(winner === 'Player') {
         playerWins += 1;
       } else if (winner === 'Computer') {
         computerWins += 1;
       }
-      console.log("Player chose " + playerTurn + " Computer chose " + computerTurn);
-      console.log(winner + " Won this round!");
+
+      console.log("Player chose " + playerMove + ", Computer chose " + computerMove);
+
+      if(winner === 'Player' || winner === 'Computer') {
+        console.log(winner + " Won this round!");
+      } else if (winner === 'tie') {
+        console.log("It was a tie!");
+      }
+
+     console.log("The score currently is: " + '\n' +
+                "Player has scored " + playerWins + '\n' +
+                "Computer has scored " + computerWins);
     }
-    if (playerWins >= 5 && computerWins < 5) {
-        return console.log("Player has won this game.");
-    } else {
-        return console.log("computer has won this game.");
+
+    if (playerWins === 5 && computerWins < 5) {
+        console.log("Player has won this game.");
+    } else{
+        console.log("computer has won this game.");
     }
 }
